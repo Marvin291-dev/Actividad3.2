ecd0c679ce046e381309833c37b3e46054296939
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-07-08 17:15:38 -0600

    agregar mascota

--- Actividad3.py
+++ Actividad3.py
@@ -4,3 +4,6 @@ class Cliente:
         self.telefono = telefono
         self.correo = correo
         self.mascotas = []
+
+        def agregar_mascota(self, mascota):
+            self.mascotas.append(mascota)
